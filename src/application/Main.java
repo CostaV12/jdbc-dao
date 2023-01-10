e9257acfeccf63d0d5b68cee87691998ba7dc9d8
Vini Costa <vc896536@gmail.com>
2023-01-10 17:57:05 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,8 @@ public class Main {
     public static void main(String[] args) {
         Department department = new Department(1, "Computers");
         Seller seller = new Seller(1, "Bob Brown", "bob@gmail.com", LocalDate.now(), 2000.00, department);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(department);
         System.out.println(seller);
