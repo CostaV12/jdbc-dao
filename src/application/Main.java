daf571ded097ec254f66b2cc2429708a79143170
Vini Costa <vc896536@gmail.com>
2023-01-11 10:19:26 -0300

    Adding findById implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,12 +9,10 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Department department = new Department(1, "Computers");
-        Seller seller = new Seller(1, "Bob Brown", "bob@gmail.com", LocalDate.now(), 2000.00, department);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
-        System.out.println(department);
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class SellerDaoJDBC implements SellerDao {
+
+    private Connection connection;
+
+    public SellerDaoJDBC(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
     public void insert(Seller seller) {
 
@@ -18,12 +31,40 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void deleteById(Integer id) {
-
     }
 
     @Override
     public Seller findById(Integer id) {
-        return null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            preparedStatement = connection.prepareStatement("SELECT s.*,d.Name as DepName "
+                    + "FROM seller AS s INNER JOIN department AS d "
+                    + "ON s.DepartmentId = d.Id "
+                    + "WHERE s.Id = ?");
+
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                Department department = new Department(resultSet.getInt("DepartmentId"), resultSet.getString("DepName"));
+                return new Seller(
+                        resultSet.getInt("Id"),
+                        resultSet.getString("Name"),
+                        resultSet.getString("Email"),
+                        resultSet.getDate("BirthDate").toLocalDate(),
+                        resultSet.getDouble("BaseSalary"),
+                        department);
+            } else {
+                return null;
+            }
+
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+
     }
 
     @Override
