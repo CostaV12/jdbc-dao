6e1a1da6a74407026b41cfec043b29aac34fcae1
Vini Costa <vc896536@gmail.com>
2023-01-12 16:33:24 -0300

    Implementing DepartmentDaoJDBC

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,60 +1,43 @@
 package application;
 
 import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        SellerDao sellerDao = DaoFactory.createSellerDao();
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
-        Seller seller = sellerDao.findById(3);
 
-        System.out.println("----- TESTE findByID: -----");
-        System.out.println(seller);
+        System.out.println("---TEST: INSERT---");
+        Department department = new Department(null, "RH");
+        departmentDao.insert(department);
+        System.out.println("Id: " + department.getId());
 
-        System.out.println();
+        System.out.println("---TEST: UPDATE---");
+        department.setName("People");
+        departmentDao.update(department);
+        System.out.println("Updated");
 
-        System.out.println("----- TESTE findByDepartment: -----");
-        List<Seller> sellers = sellerDao.findByDepartment(2);
-        for (Seller seller1 : sellers) {
-            System.out.println(seller1);
+        System.out.println("---TEST: DELETE---");
+        departmentDao.deleteById(5);
+        System.out.println("Deleted");
+
+        System.out.println("---TEST: FIND BY ID---");
+        department = departmentDao.findById(6);
+        System.out.println(department);
+
+        System.out.println("---TEST: FIND ALL---");
+        List<Department> departments = departmentDao.findAll();
+        for (Department department1 : departments) {
+            System.out.println(department1);
         }
 
-        System.out.println();
-
-        System.out.println("----- TESTE findAll: -----");
-        sellers = sellerDao.findAll();
-        for (Seller seller1 : sellers) {
-            System.out.println(seller1);
-        }
-
-        System.out.println();
-
-        System.out.println("----- TESTE insert: -----");
-
-        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", LocalDate.now(), 4000.0, new Department(2, null));
-        sellerDao.insert(newSeller);
-        System.out.println("New Seller, id = " + newSeller.getId());
-
-        System.out.println();
-
-        System.out.println("----- TESTE update: -----");
-
-        Seller updateSeller = sellerDao.findById(9);
-        updateSeller.setName("Vinicius Costa dos Santos");
-        sellerDao.update(updateSeller);
-        System.out.println("Updated seller");
-
-        System.out.println();
-
-        System.out.println("----- TESTE delete: -----");
-
-        sellerDao.deleteById(4);
-        System.out.println("Deleted seller");
     }
 }
